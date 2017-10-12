7fe538f86e520a7690adf4d00c52c2ce58969a8d
Kelvin <kelvinmaus@gmail.com>
2017-10-11 23:04:23 -0300

    implementação de leitura de limite hidrico máximo diário e funcões de cálculo movidas para dentro do listener

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,7 @@ admin.initializeApp(functions.config().firebase);
 // [END import]
 const limiteSodioDiarioPadrao = 4;
 const limiteFosforoDiarioPadrao = 800;
-const limitePotassioDiarioPadrao = 1000;
-const limiteHidricoDiarioPadrao = 500;
-const limiteHidricoDiarioTotal = functions.database.ref('/users/{userId}/controle_hidrico/limite_hidrico_diario_total').on('value', function(snapshot) {
-  return snapshot.val();
-});
+const limitePotassioDiarioPadrao = 2000;
 
 // [START percentualNutricional Function]
 const percentualNutricional = (fosforo, potassio, sodio) => {
@@ -29,16 +25,16 @@ const percentualNutricional = (fosforo, potassio, sodio) => {
   return percentualAtingido;
 }
 
-// [START percentualHidrico Function]
-const percentualHidrico = (hidrico) => {
+//------- [START percentualHidrico Function]---------
+const percentualHidrico = (hidrico, limiteHidrico = 500) => {
+  var percentualHidricoAtingido = hidrico/limiteHidrico;
+  percentualHidricoAtingido = percentualHidricoAtingido.toPrecision(2)
 
-  const percentualHidricoAtingido = hidrico/limiteHidricoDiarioPadrao;
-
-  return percentualHidricoAtingido.toPrecision(2);
+  return percentualHidricoAtingido;
 }
 // [END percentualHidrico Function]
 
-// [START calculoLimitesDiarios]
+//------ [START calculoLimitesDiarios]---------------
 // Listens for new user insertions added to /users/{userId}/consumo_alimentar/{date} and calculate
 // daily limits and write the new data to /users/{userId}/limites_diarios/{date}
 exports.calculoLimitesDiarios = functions.database.ref('/users/{userId}/consumo_alimentar/{date}')
@@ -48,55 +44,56 @@ exports.calculoLimitesDiarios = functions.database.ref('/users/{userId}/consumo_
     var potassioTotalDiario = 0;
     var sodioTotalDiario = 0;
     var hidricoTotalDiario = 0;
-
-    // Obtem o Expo token do usuário
+    // Obtem o ID Firebase do usuário
     const userUid = event.params.userId;
 
-		console.log('Usuário ID', event.params.userId)
-    console.log('Event Params Date', event.params.date)
-    console.log('Data', event.data)
+    return (
+      admin.database().ref(`/users/${userUid}/controle_hidrico/limite_hidrico_diario_total`).once('value').then(function(snapshot){
 
-    const json = event.data.val();
-    //const arrJSON = [];
-    for (var obj in json){
-      fosforoTotalDiario += json[obj].nutrientes_consumidos.fosforoTotal
-      potassioTotalDiario += json[obj].nutrientes_consumidos.potassioTotal
-      sodioTotalDiario += (json[obj].nutrientes_consumidos.sodioTotal)/1000.0
-      hidricoTotalDiario += json[obj].volume_hidrico_consumido
-      //arrJSON.push(json[obj]);
-    }
-    //console.log('arrJSON', arrJSON)
-    const percentualNutricionalAtingido = percentualNutricional(fosforoTotalDiario, potassioTotalDiario, sodioTotalDiario);
-    const volumePercentualAtingido = percentualHidrico(hidricoTotalDiario);
+      const limiteHidricoDiarioMaximo = snapshot.val();
 
-    const limite_nutricional = {
-      fosforo: fosforoTotalDiario.toFixed(),
-      potassio: potassioTotalDiario.toFixed(),
-      sodio: sodioTotalDiario.toFixed(1)
-    }
+      const json = event.data.val();
+      //const arrJSON = [];
+      for (var obj in json){
+        fosforoTotalDiario += json[obj].nutrientes_consumidos.fosforoTotal
+        potassioTotalDiario += json[obj].nutrientes_consumidos.potassioTotal
+        sodioTotalDiario += (json[obj].nutrientes_consumidos.sodioTotal)/1000.0
+        hidricoTotalDiario += json[obj].volume_hidrico_consumido
+        //arrJSON.push(json[obj]);
+      }
+      //console.log('arrJSON', arrJSON)
+      const percentualNutricionalAtingido = percentualNutricional(fosforoTotalDiario, potassioTotalDiario, sodioTotalDiario);
+      const volumePercentualAtingido = percentualHidrico(hidricoTotalDiario, limiteHidricoDiarioMaximo);
 
-    console.log('Nutrientes Totais => ', fosforoTotalDiario, potassioTotalDiario, sodioTotalDiario)
-    console.log('Percentual dos Nutrientes Diários =>', percentualNutricionalAtingido.fosforo, percentualNutricionalAtingido.potassio, percentualNutricionalAtingido.sodio)
-    console.log('Volume Hídrico Diário =>', volumePercentualAtingido + ' %', hidricoTotalDiario + ' ml')
+      const limite_nutricional = {
+        fosforo: fosforoTotalDiario.toFixed(),
+        potassio: potassioTotalDiario.toFixed(),
+        sodio: sodioTotalDiario.toFixed(1)
+      }
 
-    const limites_diarios = {
-      limite_nutricional: {
-        limite_percentual_atingido: percentualNutricionalAtingido,
-        limite_nutricional_atingido: limite_nutricional,
+      console.log('Nutrientes Totais => ', fosforoTotalDiario, potassioTotalDiario, sodioTotalDiario)
+      console.log('Percentual dos Nutrientes Diários =>', percentualNutricionalAtingido.fosforo, percentualNutricionalAtingido.potassio, percentualNutricionalAtingido.sodio)
+      console.log('Volume Hídrico Diário =>', volumePercentualAtingido + '%', hidricoTotalDiario + ' ml')
+
+      const limites_diarios = {
+        limite_nutricional: {
+          limite_percentual_atingido: percentualNutricionalAtingido,
+          limite_nutricional_atingido: limite_nutricional,
+          limite_percentual_hidrico:  volumePercentualAtingido,
+          limite_volume_hidrico: hidricoTotalDiario
+        },
         limite_percentual_hidrico:  volumePercentualAtingido,
         limite_volume_hidrico: hidricoTotalDiario
-      },
-      limite_percentual_hidrico:  volumePercentualAtingido,
-      limite_volume_hidrico: hidricoTotalDiario
-    };
+      };
 
-    console.log(limites_diarios);
-
-    return admin.database().ref('/users/' + event.params.userId + '/limites_diarios/' + event.params.date + '/').set(limites_diarios);
+      admin.database().ref(`/users/${event.params.userId}/limites_diarios/${event.params.date}/`).set(limites_diarios);
+    })
+  )
 });
-// [END makeUppercase]
 
-// [START limiteHidricoDiario]
+// [END calculoLimitesDiarios]
+
+// [START calculoLimiteHidricoDiarioTotal]
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
 exports.calculoLimiteHidricoDiarioTotal = functions.database.ref('/users/{userId}/controle_hidrico')
@@ -112,5 +109,5 @@ exports.calculoLimiteHidricoDiarioTotal = functions.database.ref('/users/{userId
       //return admin.database().ref('/users/' + event.params.userId + '/limite_hidrico_diario/').set(limiteHidrico);
       return event.data.ref.child('limite_hidrico_diario_total').set(limiteHidrico);
     });
-// [END limiteHidricoDiario]
+// [END calculoLimiteHidricoDiarioTotal]
 // [END all]
